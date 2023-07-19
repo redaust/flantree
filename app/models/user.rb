c161ac06d9dbebb708db0f252166122d31c821a5
Austin Marsh <redausty@live.com>
2023-07-19 18:36:12 -0400

    adding usernames, bios, and full name field to an acccount

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+
+  validates :full_name, length: { maximum: 40 }
+  validates :body, length: { maximum: 80 }
+  validate :valid_username
+
+  def valid_username
+    errors.add(:username, "is already taken") if User.exists?(username: username)
+
+    restricted_username_list = %[admin root dashboard analytics appearance settings preferences calendar]
+
+    errors.add(:username, "is restricted") if restricted_username_list.include?(username)
+  end
 end
